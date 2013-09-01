430cf8637a019b33973c49007e2f2fedb49ccdc8
samerlahoud <lahoud.samer@gmail.com>
2013-09-01 17:26:58 +0200

    add licence

--- star.py
+++ star.py
@@ -1,6 +1,9 @@
 #### Webapp that indicates the next rounds of Rennes STAR bus lines at "Tournebride" towards "Republique"
 #### Deployed on http://bus-tournebride.appspot.com/
 #### Can be added as a bookmark or a quick launch app on smartphones, PCs, ...
+### BEGIN LICENSE
+# This file is in the public domain
+### END LICENSE
 
 import webapp2
 from datetime import datetime
